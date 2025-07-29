14937c3441ce5ea4cae6aa236bcd8e9200b96152
White, Ryan <ryanwhite0816@gmail.com>
2025-07-29 10:34:51 -0500

    p9 and p20

---
+++ p20_isValid.py
@@ -0,0 +1,15 @@
+
+class Solution(object):
+    def isValid(self, strs:s) -> bool:
+        stack = []
+        bracket_map = {')':'(', '}':'{', ']':'['}
+
+        for char in s:
+            bracket_map[char] in 
+        
+
+
+sol = Solution()
+s = "()"
+ouput = sol.isValid(s)
+print(ouput)
---
+++ p9_isPalindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self,x):
+        
+
+
+sol = Solution()
+x = 121
+ouput = sol.isPalindrome(x)
+print(ouput)
